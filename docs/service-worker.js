11b40c9d5de617b803e4a6312c333a4c5bac3247
Kayvan Arianpour <karianpour@gmail.com>
2020-08-31 17:11:03 +0430

    building

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6d9b747183a6fb90e3b7a6baa48d801a.js"
+  "/precache-manifest.c21edcc4ec221a626186bd866a572d37.js"
 );
 
 self.addEventListener('message', (event) => {
---
+++ docs/static/js/5.a64f4ad9.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpk1weather=this.webpackJsonpk1weather||[]).push([[5],{64:function(t,r,e){"use strict";e.r(r);var o=e(0),a=e.n(o),c=e(17),n=Object(c.a)((function(t){return{root:{backgroundColor:t.colorPrimary,color:t.textPrimary}}}));r.default=function(){var t=Object(c.c)(),r=n({theme:t});return a.a.createElement("div",{className:r.root},"404 - Not Found")}}}]);
+//# sourceMappingURL=5.a64f4ad9.chunk.js.map

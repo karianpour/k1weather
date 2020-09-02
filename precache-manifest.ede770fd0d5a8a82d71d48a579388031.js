12de646f4cd7071e6477249215a4d84c0e2c29c5
Kayvan Arianpour <karianpour@gmail.com>
2020-09-02 09:08:03 +0430

    Updates

---
+++ precache-manifest.ede770fd0d5a8a82d71d48a579388031.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "268cd222b8c80017be46e303e39a7379",
+    "url": "/k1weather/index.html"
+  },
+  {
+    "revision": "ecafe31bea4a0d408ded",
+    "url": "/k1weather/static/js/0.596d8fe5.chunk.js"
+  },
+  {
+    "revision": "6c4a10ec54f4b3464c88",
+    "url": "/k1weather/static/js/3.c11d8d25.chunk.js"
+  },
+  {
+    "revision": "bb0d2e924b7b6743d70dbdc0ece34a4f",
+    "url": "/k1weather/static/js/3.c11d8d25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8722823175845086727e",
+    "url": "/k1weather/static/js/4.ef11a645.chunk.js"
+  },
+  {
+    "revision": "de6f03b3f8bbb3c1aa2d",
+    "url": "/k1weather/static/js/5.16c3b6a3.chunk.js"
+  },
+  {
+    "revision": "403ab34db8b31b150170",
+    "url": "/k1weather/static/js/6.25050ada.chunk.js"
+  },
+  {
+    "revision": "a182e0d690f20a4f44b7",
+    "url": "/k1weather/static/js/main.a0853393.chunk.js"
+  },
+  {
+    "revision": "04daf69f9d7d9bdbf6ff",
+    "url": "/k1weather/static/js/runtime-main.5a2acbed.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/k1weather/precache-manifest.ede770fd0d5a8a82d71d48a579388031.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/k1weather/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});

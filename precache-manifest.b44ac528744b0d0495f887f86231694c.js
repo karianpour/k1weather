9fa2361d4320f43fedb81d76bf422e6e67d697e5
Kayvan Arianpour <karianpour@gmail.com>
2020-09-05 16:36:09 +0430

    Updates

---
+++ precache-manifest.b44ac528744b0d0495f887f86231694c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2b966469097702100a41b728c48fb01",
+    "url": "/k1weather/index.html"
+  },
+  {
+    "revision": "00da5a5304aeee761dbc",
+    "url": "/k1weather/static/js/0.7c741aac.chunk.js"
+  },
+  {
+    "revision": "7787b1a54471e5297ec3",
+    "url": "/k1weather/static/js/3.0652029c.chunk.js"
+  },
+  {
+    "revision": "bb0d2e924b7b6743d70dbdc0ece34a4f",
+    "url": "/k1weather/static/js/3.0652029c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f89fb908e88ba96b8915",
+    "url": "/k1weather/static/js/4.f13e8c74.chunk.js"
+  },
+  {
+    "revision": "9a355b711b8b4002f63c",
+    "url": "/k1weather/static/js/5.416d57db.chunk.js"
+  },
+  {
+    "revision": "ac4779381c0b692224d0",
+    "url": "/k1weather/static/js/6.7fa327a8.chunk.js"
+  },
+  {
+    "revision": "42fc71c7a34d27bba272",
+    "url": "/k1weather/static/js/7.29971295.chunk.js"
+  },
+  {
+    "revision": "effeb19beff18e288f93",
+    "url": "/k1weather/static/js/main.12e39a20.chunk.js"
+  },
+  {
+    "revision": "32114c8688ae4e10c4e3",
+    "url": "/k1weather/static/js/runtime-main.1923848c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/k1weather/precache-manifest.9d8f12b84cbc1302ec2bd4b54d39373c.js"
+  "/k1weather/precache-manifest.b44ac528744b0d0495f887f86231694c.js"
 );
 
 self.addEventListener('message', (event) => {
